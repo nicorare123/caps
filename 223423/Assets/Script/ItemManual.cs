1423592356e900269fbb1075035291b4852a7223
bluesmile <sungwi999@gmail.com>
2024-10-14 20:30:02 +0900

    material
    material

--- 223423/Assets/Script/ItemManual.cs
+++ 223423/Assets/Script/ItemManual.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ItemManual : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Sprite grapeSprite;  // 포도 스프라이트를 할당할 변수
+    public Image targetImage;   // 비교할 이미지가 담긴 UI 오브젝트
 
-    // Update is called once per frame
-    void Update()
+    // 마우스가 UI 이미지에 들어왔을 때 호출됨
+  
+    public void Itembutton()
     {
-        
+        // 이미지의 스프라이트가 포도 스프라이트와 일치하는지 확인
+        if (targetImage != null && targetImage.sprite == grapeSprite)
+        {
+            Debug.Log("김치");
+        }
     }
 }
